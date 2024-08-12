eeed444497b736695d8d22b4f9e724444eee8d76
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-12 18:26:15 +0200

    Add items to seed in the database

---
+++ db/seed.js
@@ -0,0 +1,55 @@
+const pool = require('./pool');
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS product(
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    product_name VARCHAR(255) UNIQUE,
+    quantity INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    category_name VARCHAR(255) UNIQUE,
+    product_id INTEGER,
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
+
+INSERT INTO product ("product_name", "quantity")
+    VALUES ('biscuits', 2)
+    ON CONFLICT (product_name) DO NOTHING;
+
+INSERT INTO product ("product_name", "quantity")
+    VALUES ('Mercedes Benz C200', 4)
+    ON CONFLICT (product_name) DO NOTHING;
+
+INSERT INTO categories (category_name, product_id)
+    VALUES ('snacks', 1)
+    ON CONFLICT (category_name) DO NOTHING;
+
+INSERT INTO categories (category_name, product_id)
+    VALUES ('Cars', 9)
+    ON CONFLICT (category_name) DO NOTHING;
+
+`;
+
+
+
+async function main() {
+    try {
+        console.log("seeding.....");
+        await pool.query(SQL);
+        console.log("seeding complete");
+    } catch (error) {
+        console.error("An error occurred while seeding the database", error);
+    } finally {
+        pool.end();
+    }
+}
+
+main();
