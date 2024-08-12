cb54e2969d6851bc2a4997bf1c55719be85ca497
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-12 13:13:11 +0200

    Add the logic to render categories

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
+const db = require("../db/queries")
 
 exports.renderIndex = asyncHandler(async (req, res) => {
-  res.render("index");
+  const categories = await db.getAllCategories()
+  res.render("index", {title:'Categories',categories:categories} );
 });
