2ecc15bd0809d1d42e3895f36dac071b8b126fa4
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-13 11:27:58 +0200

    Add the id field from the select method

--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ const pool = require('./pool')
 
 
  async function getAllCategories(){
-    const categories = await pool.query("SELECT category_name FROM categories")
+    const categories = await pool.query("SELECT id, category_name FROM categories")
     return categories.rows;
 }
 
