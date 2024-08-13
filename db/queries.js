44ca993a6637f5641d2c91754a81434fcf63322c
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-13 17:27:41 +0200

    Add query that will insert new items into the database

--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ const pool = require("./pool");
 
 async function getAllCategories() {
   const categories = await pool.query(
-    "SELECT id, category_name FROM categories",
+    "SELECT DISTINCT id, category_name FROM categories",
   );
   return categories.rows;
 }
@@ -16,13 +16,29 @@ async function getAllItemsForCategory(id) {
   return items.rows;
 }
 
-async function postItem(name, quantity) {
-    await pool.query("INSERT INTO product(product_name, quantity) VALUES ($1, $2)", [name, quantity]);
-    return "Success"
+async function postItem(name, quantity, category) {
+  await pool.query(
+    "INSERT INTO product(product_name, quantity) VALUES ($1, $2)",
+    [name, quantity],
+  );
+  const itemIdRow = await pool.query(
+    "SELECT id FROM product WHERE product_name = $1",
+    [name],
+  );
+  const itemId = itemIdRow.rows[0].id;
+  await pool.query(
+    "INSERT INTO categories (category_name, product_id) VALUES ($1, $2)",
+    [category, itemId],
+  );
+  const categoryId = await pool.query(
+    "SELECT id FROM categories WHERE product_id = $1",
+    [itemId],
+  );
+  return categoryId.rows[0].id;
 }
 
 module.exports = {
   getAllCategories,
   getAllItemsForCategory,
-  postItem
+  postItem,
 };
