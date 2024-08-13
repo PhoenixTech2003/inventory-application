97e90baffafa50cb4fe6a93b444199360083e081
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-13 20:42:32 +0200

    refactor: Update addItemsRoutes to use category instead of id for rendering and inserting items

---
+++ routes/addItemsRoutes.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const controller = require("../controllers/addItemController");
+const addItemsRouter = Router();
+
+addItemsRouter.get("/", controller.renderAddItems);
+addItemsRouter.post("/", controller.insertProducts)
+module.exports = addItemsRouter;
