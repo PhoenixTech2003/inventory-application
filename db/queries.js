4df011da52851985456998ed9c4f6cc44ffb4469
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-13 12:16:33 +0200

    Add query that selects all items that belong a particular category

--- db/queries.js
+++ db/queries.js
@@ -1,11 +1,22 @@
-const pool = require('./pool')
+const pool = require("./pool");
 
+async function getAllCategories() {
+  const categories = await pool.query(
+    "SELECT id, category_name FROM categories",
+  );
+  return categories.rows;
+}
 
- async function getAllCategories(){
-    const categories = await pool.query("SELECT id, category_name FROM categories")
-    return categories.rows;
+async function getAllItemsForCategory(id) {
+  const items = await pool.query(
+    "SELECT product_name , quantity, category_name FROM product join categories on product.id = categories.id WHERE categories.id = $1;",
+    [id],
+  );
+
+  return items.rows;
 }
 
 module.exports = {
-    getAllCategories
-}
+  getAllCategories,
+  getAllItemsForCategory
+};
