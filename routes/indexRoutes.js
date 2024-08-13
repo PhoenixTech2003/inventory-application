06db5b862f4150a0dd269cfc514ea59476f64d0c
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-13 12:15:09 +0200

    Add the items route

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const controller = require("../controllers/indexControllers");
 
+
 const indexRouter = Router();
 
 indexRouter.get("/", controller.renderIndex);
+indexRouter.get("/items/:id", controller.renderItems)
 
 module.exports = indexRouter;
