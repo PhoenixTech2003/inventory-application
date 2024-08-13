ed72ced39529b26af3263a205515f96e973926dc
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-13 12:15:48 +0200

    Add conroller to select all items that belong to a particular category

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -5,3 +5,10 @@ exports.renderIndex = asyncHandler(async (req, res) => {
   const categories = await db.getAllCategories()
   res.render("index", {title:'Categories',categories:categories} );
 });
+
+exports.renderItems =asyncHandler(async(req, res)=>{
+  const id = req.params.id;
+  const items = await db.getAllItemsForCategory(id);
+  const categoryName = items[0].category_name
+  res.render("items",{title:categoryName,items:items} )
+})
