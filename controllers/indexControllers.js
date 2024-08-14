04accc2d0e79b9f90f5bbe887215410a4a43fddb
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-14 07:13:45 +0200

    Remove the console logs

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -10,12 +10,12 @@ exports.renderItems = asyncHandler(async (req, res) => {
   const category = req.params.category;
 
   const items = await db.getAllItemsForCategory(category);
-  console.log(items);
+  
   if (items.length === 0) {
     res.render("items", { title: category, items: [] });
     return;
   }
   const categoryName = items[0].category_name;
-  console.log(categoryName);
+
   res.render("items", { title: categoryName, items: items });
 });
