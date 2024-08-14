8debdf351740e51b00081abb6ca5e941414551eb
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-14 13:20:41 +0200

    feat: Add server-side validation for updating items

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -10,6 +10,18 @@ const validateCategory = [
   body("category_name").trim().notEmpty().withMessage("Please enter a value"),
 ];
 
+const validateItem = [
+  body("product_name")
+    .trim()
+    .notEmpty()
+    .withMessage("Product name must have a value"),
+  body("quantity")
+    .trim()
+    .isNumeric()
+    .withMessage("Quantity must contain numbers only"),
+  body("category").trim().notEmpty().withMessage("Please select a category"),
+];
+
 exports.renderIndex = asyncHandler(async (req, res) => {
   const categories = await db.getAllCategories();
   res.render("index", { title: "Categories", categories: categories });
@@ -57,16 +69,55 @@ exports.updateCategory = [
     const currentCategoryName = req.body.category;
     const newCategoryName = req.body.category_name;
     if (!errors.isEmpty()) {
-      return res
-        .status(400)
-        .render("updateCategory", {
-          title: "Update Category",
-          categoryName: currentCategoryName,
-          errors: errors.array(),
-        });
+      return res.status(400).render("updateCategory", {
+        title: "Update Category",
+        categoryName: currentCategoryName,
+        errors: errors.array(),
+      });
     }
 
     await db.updateCategory(newCategoryName, currentCategoryName);
     res.redirect("/");
+  }),
+];
+
+exports.renderUpdateItem = asyncHandler(async (req, res) => {
+  const itemName = req.query.item;
+  const currentCategory = req.query.category;
+  const categories = await db.getAllCategories();
+  res.render("updateItem", {
+    title: "Update Item",
+    itemName: itemName,
+    categories: categories,
+    currentCategory: currentCategory,
+  });
+});
+
+exports.updateItem = [
+  validateItem,
+  asyncHandler(async (req, res) => {
+    const categories = await db.getAllCategories();
+    const currentItemName = req.body.current_item_name;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res
+        .status(400)
+        .render("updateItem", {
+          title: "Upadte Item",
+          categories: categories,
+          itemName: currentItemName,
+          errors: errors.array(),
+        });
+    }
+    const newProductName = req.body.product_name;
+    const newQuantity = req.body.quantity;
+    const newCategory = req.body.category;
+    await db.updateItem(
+      newProductName,
+      newQuantity,
+      currentItemName,
+      newCategory,
+    );
+    res.redirect(`/items/${newCategory}`);
   }),
 ];
