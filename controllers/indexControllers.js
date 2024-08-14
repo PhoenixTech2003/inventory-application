5471dc090175770cb95a583ee3e4d11383192ce3
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-14 07:04:19 +0200

    refactor: Update renderItems function to handle empty item list and improve category title rendering

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,14 +1,21 @@
 const asyncHandler = require("express-async-handler");
-const db = require("../db/queries")
+const db = require("../db/queries");
 
 exports.renderIndex = asyncHandler(async (req, res) => {
-  const categories = await db.getAllCategories()
-  res.render("index", {title:'Categories',categories:categories} );
+  const categories = await db.getAllCategories();
+  res.render("index", { title: "Categories", categories: categories });
 });
 
-exports.renderItems =asyncHandler(async(req, res)=>{
+exports.renderItems = asyncHandler(async (req, res) => {
   const category = req.params.category;
+
   const items = await db.getAllItemsForCategory(category);
-  const categoryName = items[0].category_name
-  res.render("items",{title:categoryName,items:items} )
-})
+  console.log(items);
+  if (items.length === 0) {
+    res.render("items", { title: category, items: [] });
+    return;
+  }
+  const categoryName = items[0].category_name;
+  console.log(categoryName);
+  res.render("items", { title: categoryName, items: items });
+});
