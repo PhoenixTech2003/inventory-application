5e4ecdb461c71788258407ce5d8e29fe307888c2
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-14 11:23:13 +0200

    refactor: Add server-side validation for updating categories

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,5 +1,14 @@
 const asyncHandler = require("express-async-handler");
+const {
+  body,
+  vijalidationResult,
+  validationResult,
+} = require("express-validator");
 const db = require("../db/queries");
+
+const validateCategory = [
+  body("category_name").trim().notEmpty().withMessage("Please enter a value"),
+];
 
 exports.renderIndex = asyncHandler(async (req, res) => {
   const categories = await db.getAllCategories();
@@ -10,7 +19,7 @@ exports.renderItems = asyncHandler(async (req, res) => {
   const category = req.params.category;
 
   const items = await db.getAllItemsForCategory(category);
-  
+
   if (items.length === 0) {
     res.render("items", { title: category, items: [] });
     return;
@@ -20,15 +29,44 @@ exports.renderItems = asyncHandler(async (req, res) => {
   res.render("items", { title: categoryName, items: items });
 });
 
-exports.deleteItem = asyncHandler(async(req, res)=>{
+exports.deleteItem = asyncHandler(async (req, res) => {
   const itemName = req.body.product_name;
   const categoryName = req.body.category_name;
-  await db.deleteItem(itemName)
-  res.redirect(`/items/${categoryName}`)
-})
+  await db.deleteItem(itemName);
+  res.redirect(`/items/${categoryName}`);
+});
 
-exports.deleteCategory = asyncHandler(async(req,res)=>{
+exports.deleteCategory = asyncHandler(async (req, res) => {
   const categoryName = req.body.category_name;
-  await db.deleteCategory(categoryName)
-  res.redirect("/")
-})
+  await db.deleteCategory(categoryName);
+  res.redirect("/");
+});
+
+exports.renderUpdateCategory = asyncHandler(async (req, res) => {
+  const categoryName = req.query.category;
+  res.render("updateCategory", {
+    title: "Update Category",
+    categoryName: categoryName,
+  });
+});
+
+exports.updateCategory = [
+  validateCategory,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    const currentCategoryName = req.body.category;
+    const newCategoryName = req.body.category_name;
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("updateCategory", {
+          title: "Update Category",
+          categoryName: currentCategoryName,
+          errors: errors.array(),
+        });
+    }
+
+    await db.updateCategory(newCategoryName, currentCategoryName);
+    res.redirect("/");
+  }),
+];
