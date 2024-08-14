09b0bc37961489342ab5015d4002be2ef33ea695
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-14 07:06:52 +0200

    refactor: Add route for adding categories

---
+++ routes/addCategoriesRoutes.js
@@ -0,0 +1,9 @@
+const {Router} = require("express")
+const controller = require("../controllers/addCategoryController")
+
+const addCategoryRouter = Router()
+
+addCategoryRouter.get("/", controller.renderAddCategories)
+addCategoryRouter.post("/", controller.addCategories)
+
+module.exports = addCategoryRouter
