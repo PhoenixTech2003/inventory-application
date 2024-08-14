8ed03680c07a32f7e14df128d17cc1e2407020ea
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-14 11:24:37 +0200

    feat: Add update category functionality to indexRoutes.js and updateCategory.ejs

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -8,4 +8,6 @@ indexRouter.get("/", controller.renderIndex);
 indexRouter.post("/", controller.deleteCategory)
 indexRouter.get("/items/:category", controller.renderItems)
 indexRouter.post("/items/:category", controller.deleteItem)
+indexRouter.get("/update-category", controller.renderUpdateCategory)
+indexRouter.post("/update-category", controller.updateCategory)
 module.exports = indexRouter;
