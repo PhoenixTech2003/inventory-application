16391ec3857f23b4db6ccef7907b3d2d11890173
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-14 13:22:21 +0200

    refactor: Add update item functionality to indexRoutes.js and updateItem.ejs

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const controller = require("../controllers/indexControllers");
 
-
 const indexRouter = Router();
 
 indexRouter.get("/", controller.renderIndex);
-indexRouter.post("/", controller.deleteCategory)
-indexRouter.get("/items/:category", controller.renderItems)
-indexRouter.post("/items/:category", controller.deleteItem)
-indexRouter.get("/update-category", controller.renderUpdateCategory)
-indexRouter.post("/update-category", controller.updateCategory)
+indexRouter.post("/", controller.deleteCategory);
+indexRouter.get("/items/:category", controller.renderItems);
+indexRouter.post("/items/:category", controller.deleteItem);
+indexRouter.get("/update-category", controller.renderUpdateCategory);
+indexRouter.post("/update-category", controller.updateCategory);
+indexRouter.get("/update-item", controller.renderUpdateItem);
+indexRouter.post("/update-item", controller.updateItem);
 module.exports = indexRouter;
